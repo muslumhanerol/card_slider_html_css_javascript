d550d4a6df542b9473766de5ac77441c3a5c168f
muslumhanerol <132482365+muslumhanerol@users.noreply.github.com>
2025-02-18 22:50:38 +0300

    media_query

--- js/script.js
+++ js/script.js
@@ -14,4 +14,19 @@ var swiper = new Swiper(".slide-content", {
         nextEl: ".swiper-button-next",
         prevEl: ".swiper-button-prev",
     },
+
+    breakpoints: {
+        0: {
+            slidesPerView: 1,
+
+        },
+        520: {
+            slidesPerView: 2,
+
+        },
+        950: {
+            slidesPerView: 3,
+
+        },
+    },
 });
